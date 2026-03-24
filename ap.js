7499257dd8913eeb70e14e3f9d354b419502364e
qobiljonovb27@gmail.com <qobiljonovb27@gmail.com>
2026-03-24 18:01:26 +0500

    Yomon

---
+++ ap.js
@@ -0,0 +1,28 @@
+const counters = document.querySelectorAll('.counter' );
+counters.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+        const increment = target / 200;
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
+});
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = { threshold: 0.1 };
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) return;
+        entry.target.classList.add('show');
+        appearOnScroll.unobserve(entry.target);
+    });
+}, appearOptions);
+
+faders.forEach(fader => appearOnScroll.observe(fader));
