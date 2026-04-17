6109d1a75caa1efca5b3fe51494011d4cf4378a3
qobiljonovb27@gmail.com <qobiljonovb27@gmail.com>
2026-04-17 19:52:17 +0500

    w

--- ap.js
+++ ap.js
@@ -1,28 +1,40 @@
-const counters = document.querySelectorAll('.counter' );
-counters.forEach(counter => {
-    counter.innerText = '0';
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target');
-        const c = +counter.innerText;
-        const increment = target / 200;
-        if(c < target){
-            counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter, 10);
-        } else {
-            counter.innerText = target;
-        }
-    };
-    updateCounter();
+
+const counters = document.querySelectorAll(".counter");
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+    const increment = target / 200;
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
 });
-const faders = document.querySelectorAll('.fade-in');
+const faders = document.querySelectorAll(".fade-in");
 const appearOptions = { threshold: 0.1 };
 
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) return;
-        entry.target.classList.add('show');
-        appearOnScroll.unobserve(entry.target);
-    });
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll,
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("show");
+    appearOnScroll.unobserve(entry.target);
+  });
 }, appearOptions);
 
-faders.forEach(fader => appearOnScroll.observe(fader));
+faders.forEach((fader) => appearOnScroll.observe(fader));
+
+
+
+
+let username = promt("Ismingizni Kiriting");
+if (username.includes("A")) {
+  console.log(Sizni);
+}
